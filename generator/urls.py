fda0bbac192ea03954f5842c1e75d5a33cdf454d
Bhuvneshja <bhuvneshjain2000@gmail.com>
2024-08-03 17:52:42 +0530

    Password Generator Project Completed

--- generator/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('password_generator/<str:username>/', views.password_generator, name='password_generator'),
-    path('result_password/', views.result_password, name='result_password'),
-    path('logout/', views.logout, name='logout'),
-]
--- password_generator_django/password_generator_django/generator/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('password_generator/<str:username>/', views.password_generator, name='password_generator'),
-    path('logout/', views.logout, name='logout'),
-]
