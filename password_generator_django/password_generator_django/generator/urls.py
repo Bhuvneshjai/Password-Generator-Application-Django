b2e83cd8cb075fa7d26b2a57b452c520d78f28b7
Bhuvneshja <bhuvneshjain2000@gmail.com>
2024-08-03 17:37:30 +0530

    Project Setup Successfully

--- password_generator_django/password_generator_django/generator/urls.py
+++ password_generator_django/password_generator_django/generator/urls.py
@@ -4,6 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('password_generator/<str:username>/', views.password_generator, name='password_generator'),
-    path('result_password/', views.result_password, name='result_password'),
     path('logout/', views.logout, name='logout'),
 ]
--- password_generator_django/password_generator_django/password_generator_django/settings.py
+++ password_generator_django/password_generator_django/password_generator_django/settings.py
@@ -116,21 +116,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-# import os 
-# STATIC_URL = '/static/'
-# STATICFILES_DIRS = [os.path.join(BASE_DIR / "generator/templates")]
-
-import os
-from pathlib import Path
-
-BASE_DIR = Path(__file__).resolve().parent.parent
-
+import os 
 STATIC_URL = '/static/'
-
-STATICFILES_DIRS = [
-    BASE_DIR / "generator/templates",
-]
-
+STATICFILES_DIRS = [os.path.join(BASE_DIR / "generator/templates")]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
